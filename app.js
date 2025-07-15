21409c1a912a953e0cba1c3817e69e1c97a8734b
barbara65-p <cerdabarbara58@gmail.com>
2025-07-15 13:21:59 -0400

    ggg

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 const formulario = document.querySelector(".from-create");
+let contador = 1;
 formulario.addEventListener("submit", (e)=> {
     e.preventDefault();
     const descripcion = document.querySelector("#descripcion").value;
     const contenedor = document.querySelector(".publicaciones");
-    contenedor.innerHTML += `<div class="publicacion">
-            <p>id: 00</p>
-            <p>${descripcion}</p>
-            <button>Editar</button>
-            <button>Eliminar</button>
+    contenedor.innerHTML += `<div id="${contador}" class="publicacion">
+        <p>id: ${contador}</p>
+        <p>${descripcion}</p>
+        <button id="editar" onclick="editar(this)">Editar</button>
+        <button id="eliminar" onclick="eliminar(this)">Eliminar</button>
         </div>`
+    ++contador;
 });
+
+
+function editar(e) {
+    
+}
+
+function eliminar(e) {
+    e.parentElement.remove();
+}
